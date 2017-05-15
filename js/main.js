f4516540dcc7405eec0f8cdcaf555a5256f3368f
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-15 14:40:28 -0400

    se crea la función que valida el nombre y que crea el span de alerta cuando no se ingresa el nombre correctamente

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+
+	function name(){
+		var idNombre = document.getElementById("name").value;
+		if (!(/^[A-Z][a-z]{3,19}$/).test(idNombre)){
+		var spanName = document.createElement("span");
+		var spanText = document.createTextNode("Nombre inválido. Ingrese nuevamente.");
+		var spanFather = document.getElementsByClassName("name-container input-box")[0];
+		spanName.appendChild(spanText);
+		spanFather.appendChild(spanName);
+		}
+	}
+	name();
+
+	
+
+
+
 }
